177174dcc33afed3455013087731dd1c71dad696
Chad Conway <agile4java@gmail.com>
2021-09-28 10:46:24 -0500

    Update webpack.config.js

--- Chapter06/Game Information App/webpack.config.js
+++ Chapter06/Game Information App/webpack.config.js
@@ -1,36 +1,35 @@
-var path = require('path');
+var path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
       {
         test: /\.scss$/,
         use: [
           {
-            loader: 'style-loader'
+            loader: "style-loader",
           },
           {
-            loader: 'css-loader'
+            loader: "css-loader",
           },
           {
-            loader: 'sass-loader'
-          }
-        ]
-      }
-    ]
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist')
-
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
   },
   performance: {
-    hints: false
+    hints: false,
   },
   devServer: {
-    publicPath: path.resolve(__dirname, './dist'),
-    writeToDisk: true
-  }
+    publicPath: path.resolve(__dirname, "./dist"),
+    writeToDisk: true,
+  },
 };
